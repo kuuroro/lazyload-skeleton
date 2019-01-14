1b07fc4de5b64a13237cbadbcca2a04d027ece5c
Javen <javenchen_sz@163.com>
2019-01-14 19:08:12 +0800

    add lazy find skeleton

--- dev/lazySkeleton.js
+++ dev/lazySkeleton.js
@@ -31,19 +31,40 @@ var lazySkeleton = (function () {
   var css = ".img {\r\n\r\n            background: linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%);\r\n            animation: loading 1.4s ease infinite;\r\n            background-size: 400% 100%;\r\n            width: 200px;\r\n            height: 100px;\r\n            display: inline-block;\r\n        }\r\n\r\n        .ylazy {\r\n            background: linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%);\r\n            animation: loading 1.4s ease infinite;\r\n            background-size: 400% 100%;\r\n            width: 200px;\r\n            height: 100px;\r\n            display: inline-block;\r\n        }\r\n\r\n        .ylazy.yskeleton {}\r\n\r\n        .ylazy.loaded {\r\n            background: none;\r\n            animation: none;\r\n        }\r\n\r\n\r\n        @keyframes loading {\r\n            0% {\r\n                background-position: 100% 50%;\r\n            }\r\n\r\n            100% {\r\n                background-position: 0 50%;\r\n            }\r\n        }\r\n\r\n\r\n\r\n\r\n\r\n        .yskeleton {\r\n            overflow: hidden;\r\n            /* border: 1px solid #f2f2f2 */\r\n        }\r\n\r\n        .yskeleton.card {\r\n            /* overflow: hidden; */\r\n            /* min-width: 200px; */\r\n            width: 140px;\r\n            display: inline-block;\r\n        }\r\n\r\n        .yskeleton.user-card {\r\n            width: 140px;\r\n            display: inline-block;\r\n\r\n        }\r\n\r\n\r\n        .yskeleton-line.user {\r\n            margin: 15px 0;\r\n            max-width: 80%;\r\n        }\r\n\r\n        .yskeleton-cover {\r\n            width: 100%;\r\n            overflow: hidden;\r\n            box-sizing: border-box;\r\n            padding: 10px;\r\n            display: flex;\r\n        }\r\n\r\n        .yskeleton.card .yskeleton-cover,\r\n        .yskeleton.user-card .yskeleton-cover {\r\n            flex-direction: column;\r\n        }\r\n\r\n\r\n\r\n        .yskeleton-user {\r\n            width: 50px;\r\n            height: 50px;\r\n            background: linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%);\r\n            animation: loading 1.4s ease infinite;\r\n            background-size: 400% 100%;\r\n            border-radius: 50%;\r\n            margin-right: 10px;\r\n            /* flex: 1; */\r\n        }\r\n\r\n        .yskeleton.user-card .yskeleton-user {\r\n            margin: 0 auto;\r\n            width: 100px;\r\n            height: 100px;\r\n            margin: 10px auto 20px;\r\n        }\r\n\r\n        .yskeleton-line-wrap {\r\n            /* overflow: hidden; */\r\n            flex: 1\r\n        }\r\n\r\n        .yskeleton-line {\r\n            background: linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%);\r\n            animation: loading 1.4s ease infinite;\r\n            background-size: 400% 100%;\r\n            height: 20px;\r\n            margin: 10px 0;\r\n        }\r\n\r\n        .yskeleton-line.small {\r\n            height: 14px;\r\n            margin: 5px 0\r\n        }\r\n\r\n        .yskeleton-card {\r\n            width: 100%;\r\n            padding-bottom: 100%;\r\n            background: linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%);\r\n            animation: loading 1.4s ease infinite;\r\n            background-size: 400% 100%;\r\n            margin: 0 auto 10px;\r\n        }\r\n\r\n\r\n        .yskeleton .content {\r\n            display: none;\r\n        }\r\n\r\n        .yskeleton.loaded .content {\r\n            display: block;\r\n        }\r\n\r\n        .yskeleton .yskeleton-cover {\r\n            display: flex;\r\n        }\r\n\r\n        .yskeleton.loaded .yskeleton-cover {\r\n            display: none;\r\n        }";
   styleInject(css);
 
-  function lazyLoad(){
+  function lazyLoad () {
       // 获取所有的图片标签
       var imgs = document.querySelectorAll('img.ylazy');
-      console.log(imgs);
+
+      function findSkeleton(node) {
+
+        
+          // if (node.tagName ) {
+          //     return false
+          // }
 
 
-      // function findSkeleton(node){
-      //     if(node)
-      // }
+          // if (cls.indexOf('yskeleton') > 0) {
+          //     return node
+          // }
+          // console.log(node,node.tagName)
 
-      // imgs.forEach(function(img){
+          if (node.tagName==='BODY') {
+              return false
+          } else {
+              var cls = node.getAttribute('class');
+              if (cls&&cls.indexOf('yskeleton') > 0) {
+                  return node
+              }
+              return findSkeleton(node.parentNode);
+          }
+          // console.log(node.tagName,cls)
 
-      // })
+      }
+
+      imgs.forEach(function (img) {
+         var b= findSkeleton(img);
+         console.log(b);
+      });
       // 获取可视区域的高度
       const viewHeight = window.innerHeight || document.documentElement.clientHeight;
       // num用于统计当前显示到了哪一张图片，避免每次都从第一张图片开始检查是否露出
@@ -55,10 +76,6 @@ var lazySkeleton = (function () {
               // 用可视区域高度减去元素顶部距离可视区域顶部的高度
               let distance = viewHeight - imgs[i].getBoundingClientRect().top;
               // 如果可视区域高度大于等于元素顶部距离可视区域顶部的高度，说明元素露出
-
-
-
-
 
               if (distance >= 0) {
                   //给元素写入真实的src，展示图片
@@ -73,6 +90,7 @@ var lazySkeleton = (function () {
           }
       }    // 监听Scroll事件
       window.addEventListener('scroll', lazyload, false);
+      // 优先首屏可是区域执行一次
       lazyload();
   }
 
--- src/lazyload.js
+++ src/lazyload.js
@@ -1,17 +1,37 @@
-
-export default function(){
+export default function () {
     // 获取所有的图片标签
     var imgs = document.querySelectorAll('img.ylazy');
-    console.log(imgs);
+
+    function findSkeleton(node) {
+
+      
+        // if (node.tagName ) {
+        //     return false
+        // }
 
 
-    // function findSkeleton(node){
-    //     if(node)
-    // }
+        // if (cls.indexOf('yskeleton') > 0) {
+        //     return node
+        // }
+        // console.log(node,node.tagName)
 
-    // imgs.forEach(function(img){
+        if (node.tagName==='BODY') {
+            return false
+        } else {
+            var cls = node.getAttribute('class');
+            if (cls&&cls.indexOf('yskeleton') > 0) {
+                return node
+            }
+            return findSkeleton(node.parentNode);
+        }
+        // console.log(node.tagName,cls)
 
-    // })
+    }
+
+    imgs.forEach(function (img) {
+       var b= findSkeleton(img)
+       console.log(b)
+    })
     // 获取可视区域的高度
     const viewHeight = window.innerHeight || document.documentElement.clientHeight
     // num用于统计当前显示到了哪一张图片，避免每次都从第一张图片开始检查是否露出
@@ -23,10 +43,6 @@ export default function(){
             // 用可视区域高度减去元素顶部距离可视区域顶部的高度
             let distance = viewHeight - imgs[i].getBoundingClientRect().top
             // 如果可视区域高度大于等于元素顶部距离可视区域顶部的高度，说明元素露出
-
-
-
-
 
             if (distance >= 0) {
                 //给元素写入真实的src，展示图片
@@ -42,5 +58,6 @@ export default function(){
     };
     // 监听Scroll事件
     window.addEventListener('scroll', lazyload, false);
+    // 优先首屏可是区域执行一次
     lazyload()
 }
